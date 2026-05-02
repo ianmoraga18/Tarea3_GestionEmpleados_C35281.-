cdd959d597dc07678992dda7f6b12f8440813b6f
Ian Moraga <ianmoraga18@gmail.com>
2026-05-01 23:16:01 -0600

    Clase empleado y AppDb

--- GestionE.Data/AppDbContext.cs
+++ GestionE.Data/AppDbContext.cs
@@ -1,7 +1,16 @@
-﻿namespace GestionE.Data
+﻿using Microsoft.EntityFrameworkCore;
+using GestionE.Models;
+
+namespace GestionE.Data
+
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Empleado> Empleados { get; set; }  
+    
 
     }
 }
--- GestionE.Models/Empleado.cs
+++ GestionE.Models/Empleado.cs
@@ -1,7 +1,32 @@
-﻿namespace GestionE.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System; 
+
+namespace GestionE.Models
 {
     public class Empleado
     {
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(80)]
+        public string Nombre { get; set; }
+
+        [Required, StringLength(100)]
+        public string Apellidos { get; set; }
+
+        [Required]
+        public string Departamento   { get; set; }
+
+        [Required, Range(400000, 10000000)]
+        public decimal Salario { get; set; }
+
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+
+        public bool Activo { get; set; } = true;    
+        public String NombreCompleto  => $"{Nombre} {Apellidos}";
+
 
     }
 }
