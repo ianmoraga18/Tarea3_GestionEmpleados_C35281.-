4ac18821f26463a0d803ec276b4da797c8aa7a42
Ian Moraga <ianmoraga18@gmail.com>
2026-05-01 23:30:00 -0600

    Implemetancion de las clases en la carpeta .BL

--- GestionE.BL/EmpleadoRepository.cs
+++ GestionE.BL/EmpleadoRepository.cs
@@ -1,10 +1,90 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using GestionE.Models;
+using GestionE.Data;
 
 namespace GestionE.BL
 {
-    internal class EmpleadoRepository
+    public class EmpleadoRepository : IEmpleadoRepository
     {
+        private readonly AppDbContext _context;
+
+        public EmpleadoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Actualizar(Empleado empleado)
+        {
+            _context.Empleados.Update(empleado);
+            _context.SaveChanges();
+        }
+
+        public void Agregar(Empleado empleado)
+        {
+            _context.Empleados.Add(empleado);
+            _context.SaveChanges();
+        }
+
+
+
+
+
+        public void Eliminar(int id)
+        {
+            var empleado = _context.Empleados.Find(id);
+            if (empleado != null)
+            {
+                empleado.Activo = !empleado.Activo;
+                _context.SaveChanges();
+            }
+        }
+
+        public int ContarTotal(string busqueda)
+        {
+
+            var query = _context.Empleados.AsQueryable();
+
+            if (!string.IsNullOrEmpty(busqueda))
+            {
+                busqueda = busqueda.ToLower();
+                query = query.Where(e => e.Nombre.ToLower().Contains(busqueda) ||
+                                         e.Apellidos.ToLower().Contains(busqueda) ||
+                                         e.Departamento.ToLower().Contains(busqueda));
+            }
+            return query.Count();
+        }
+        public IEnumerable<Empleado> ObtenerPaginado(int pagina, int tamano, string busqueda)
+        {
+            var query = _context.Empleados.AsQueryable();
+            if (!string.IsNullOrEmpty(busqueda))
+            {
+                busqueda = busqueda.ToLower();
+                query = query.Where(e => e.Nombre.ToLower().Contains(busqueda) ||
+                                         e.Apellidos.ToLower().Contains(busqueda) ||
+                                         e.Departamento.ToLower().Contains(busqueda));
+            }
+            return query.Skip((pagina - 1) * tamano).Take(tamano).ToList();
+        }
+
+
+
+        public IEnumerable<Empleado> BuscarPorNombreODepartamento(string termino)
+        {
+            termino = termino?.ToLower() ?? "";
+            return _context.Empleados
+                .Where(e => e.Nombre.ToLower().Contains(termino) ||
+                            e.Apellidos.ToLower().Contains(termino) ||
+                            e.Departamento.ToLower().Contains(termino))
+                .ToList();
+
+        }
+
+        public Empleado ObtenerPorId(int id) => _context.Empleados.Find(id);
+
+
+        public IEnumerable<Empleado> ObtenerTodos() => _context.Empleados.ToList();
+
     }
 }
--- GestionE.BL/IEmpleadoRepository.cs
+++ GestionE.BL/IEmpleadoRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace GestionE.BL
+﻿using System.Collections.Generic;
+using GestionE.Models;
+namespace GestionE.BL
 {
-    public class IEmpleadoRepository
+    public interface IEmpleadoRepository 
     {
+        IEnumerable<Empleado> ObtenerTodos();
+        Empleado ObtenerPorId(int id);
+        IEnumerable<Empleado> BuscarPorNombreODepartamento(string termino);
+        IEnumerable<Empleado> ObtenerPaginado(int pagina, int tamano, string busqueda);
+        int ContarTotal(string busqueda);
+        void Agregar(Empleado empleado);
+        void Actualizar(Empleado empleado);
+        void Eliminar(int id);
 
     }
 }
