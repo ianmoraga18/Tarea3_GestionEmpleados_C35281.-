982563b76a93a12a6e4f3c39a8558d4b425602bb
Ian Moraga <ianmoraga18@gmail.com>
2026-05-01 22:52:35 -0600

    Agregar archivos de proyecto.

---
+++ Gestion de Empleados.web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Gestion_de_Empleados.web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Gestion_de_Empleados.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
